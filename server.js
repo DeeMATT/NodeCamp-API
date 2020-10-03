699deba55cf1c5d086f8220f964b84e1bdffc2b8
CodeMask <mattode@outlook.com>
2020-10-04 00:18:11 +0100

    http requests methods for bootcamp routes

--- server.js
+++ server.js
@@ -6,12 +6,38 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api/v1/bootcamps', (req, res) => {
     res.status(200).json({
         success: true,
-        data: {
-            id: 1
-        }
+        msg: "Show all bootcamps"
+    });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Get bootcamp ${req.params.id}`
+    });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: "Create new bootcamp"
+    });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Update bootcamp ${req.params.id}`
+    });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: `Delete bootcamp ${req.params.id}`
     });
 });
 
